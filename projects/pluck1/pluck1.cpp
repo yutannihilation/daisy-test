17989abf0bced7db214b81a3b83fba281735d88a
Hiroaki Yutani <yutani.ini@gmail.com>
2020-12-13 21:24:25 +0900

    Add pluck example

---
+++ projects/pluck1/pluck1.cpp
@@ -0,0 +1,66 @@
+#include "daisy_seed.h"
+#include "daisysp.h"
+
+using namespace daisy;
+using namespace daisysp;
+
+#define BUFSIZE 256
+
+static DaisySeed seed;
+Pluck synth;
+float plkbuff[BUFSIZE];
+
+Switch button1;
+
+static void AudioCallback(float *in, float *out, size_t size)
+{
+    float sig;
+    float trig, note, decay; // Pluck Vars
+
+    button1.Debounce();
+
+    trig = 0.0f;
+    if (button1.RisingEdge())
+    {
+        trig = 1.0f;
+    }
+
+    synth.SetDecay(seed.adc.GetFloat(0));
+
+    for (size_t i = 0; i < size; i += 2)
+    {
+        synth.SetFreq(mtof(seed.adc.GetFloat(0) * 127));
+        sig = synth.Process(trig);
+
+        // left out
+        out[i] = sig;
+
+        // right out
+        out[i + 1] = sig;
+    }
+}
+
+int main(void)
+{
+    seed.Configure();
+    seed.Init();
+
+    float samplerate = seed.AudioSampleRate();
+
+    // Use pin 16 to read the voltage of the knob
+    AdcChannelConfig adcConfig;
+    adcConfig.InitSingle(seed.GetPin(16));
+    seed.adc.Init(&adcConfig, 1);
+
+    synth.Init(samplerate, plkbuff, BUFSIZE, PLUCK_MODE_RECURSIVE);
+
+    // Use pin 15 to read button to trigger the synth
+    button1.Init(seed.GetPin(15), samplerate / 48.f);
+
+    seed.adc.Start();
+    seed.StartAudio(AudioCallback);
+
+    for (;;)
+    {
+    }
+}
